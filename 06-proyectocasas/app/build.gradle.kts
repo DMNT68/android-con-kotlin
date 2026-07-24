35649641a0c37a6deedf67f7bab1d9db2665cd4a
Andres Salgado <andressalgado41@gmail.com>
2026-07-24 17:49:15 -0500

    Update: coil dependency and Kotlin version

--- 06-proyectocasas/app/build.gradle.kts
+++ 06-proyectocasas/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.material)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
