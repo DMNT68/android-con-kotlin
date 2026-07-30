c95bb54bff6f7e7b93693e251776f6fb500ad4cc
Andres Salgado <andressalgado41@gmail.com>
2026-07-30 16:55:18 -0500

    Add: Implementación de TareaViewModel para la gestión de tareas con Room

--- 08-persistencia-room/app/src/main/java/com/example/persistenciadedatosconroom/ui/TareaViewModel.kt
+++ 08-persistencia-room/app/src/main/java/com/example/persistenciadedatosconroom/ui/TareaViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.persistenciadedatosconroom.ui
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.persistenciadedatosconroom.data.Tarea
+import com.example.persistenciadedatosconroom.data.TareaDatabase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+class TareaViewModel(application: Application): AndroidViewModel(application){
+    private val dao = TareaDatabase.getDatabase(application).tareaDao()
+
+    val tareas: Flow<List<Tarea>> = dao.obtenerTareas()
+
+    fun agregarTareas(texto: String) = viewModelScope.launch { dao.insertar(Tarea(titulo = texto)) }
+
+    fun eliminarTarea(tarea: Tarea) = viewModelScope.launch { dao.eliminar(tarea) }
+
+    fun actualizarTarea(tarea: Tarea) = viewModelScope.launch { dao.actualizar(tarea) }
+
+}
